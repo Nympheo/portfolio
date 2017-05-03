e69369a0a58636ad7226abce5ce8960f38c90d10
Nympheo <synthetic.probe@gmail.com>
2017-05-03 15:10:27 +0300

    curve added

--- js/myScript.js
+++ js/myScript.js
@@ -33,17 +33,82 @@ const CONTAINER_W = parseFloat(d3.select('container').style('width'));
       return d(angles);
     });
 });})();
+//***********linear graphics********//
+(function(){
+  const data = d3.range(20).map((e) => {
+    return {
+      x: e ,
+      y: +Math.round((Math.random() * 100) + 1),
+    }
+  });
+  const margin = {top: 10, right: 10, bottom: 20, left: 20},
+    width = CONTAINER_W / 2 - margin.left - margin.right,
+    height = CONTAINER_W / 4 - margin.top - margin.bottom;
+
+  const x = d3.scaleLinear()
+      .range([0, width])
+      .domain([0, data.length]);
+
+  const y = d3.scaleLinear()
+      .rangeRound([height, 0])
+      .domain([0, d3.max(data, function(d) {return d.y})]);
+
+ const line = d3.line()
+      .x(function(d) {return x(d.x)})
+      .y(function(d) {return y(d.y)})
+      .curve(d3.curveBasis)
+
+  const svg = d3.select("#columnChild57695").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .attr('class','svgs')
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  const axisX = svg.append("g")
+      .attr("class", "axis axis--x")
+      .attr("transform", "translate(1," + height + ")")
+      .call(d3.axisBottom(x).ticks(20));
+
+  const axisY = svg.append("g")
+      .attr("class", "axis axis--y")
+      .attr("transform", "translate(" + width + ",0)")
+      .call(d3.axisLeft(y)
+              .ticks(10)
+              .tickSizeInner(width));
+
+  axisY.append("text")
+        .attr("fill", "#000")
+        .attr("transform", `rotate(-90),translate(0,${-width})`)
+        .attr("y", 3)
+        .attr("dy", "0.71em")
+        .attr("text-anchor", "end")
+        .text("Price ($)");
+  axisY.select(".domain").remove();
+  axisY.selectAll(".tick:not(:first-of-type) line")
+        .attr("stroke", "#777")
+        .attr("stroke-dasharray", "2,2");
+
+  svg.append('path')
+     .datum(data)
+     .attr('fill', 'none')
+     .attr('stroke', 'black')
+     .attr('stroke-width', 1)
+     .attr('stroke-linejoin', 'round')
+     .attr('stroke-linecap', 'round')
+     .attr('d', line);
+})();
 
 //************HISTOGRAMM*********//
 (function(){
-  const data =  [...new Array(18)]
+  const data =  [...new Array(14)]
     .map(() => Math.round(Math.random() * 100));
-  const dataX = ['rus', 'bel', 'gbr', 'ger', 'fra', 'ita', 'spa', 'usa', 'swe',
-                 'chi', 'jap', 'kor', 'ind', 'bra', 'net', 'nor', 'can', 'arg'];
+  const dataX = ['rus', 'gbr', 'ger', 'fra', 'ita', 'spa',
+  'usa', 'swe', 'chi', 'jap', 'kor', 'nor', 'can', 'arg'];
 
-  const margin = {top: 30, right: 50, bottom: 40, left: 50},
-    width = CONTAINER_W / 2 - margin.left - margin.right,
-    height = CONTAINER_W / 4 - margin.top - margin.bottom;
+  const margin = {top: 20, right: 10, bottom: 15, left: 20},
+    width = CONTAINER_W / 4 - margin.left - margin.right,
+    height = CONTAINER_W / 8 - margin.top - margin.bottom;
 
   const x = d3.scaleBand()
       .domain(dataX)
@@ -53,7 +118,7 @@ const CONTAINER_W = parseFloat(d3.select('container').style('width'));
       .domain([0, d3.max(data)])
       .range([height, 0]);
 
-  const svg = d3.select("#columnChild57695").append("svg")
+  const svg = d3.select("#columnChild56854").append("svg")
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
       .attr('class','svgs')
@@ -63,12 +128,14 @@ const CONTAINER_W = parseFloat(d3.select('container').style('width'));
   svg.append("g")
       .attr("class", "axis axis--x")
       .attr("transform", "translate(1," + height + ")")
-      .call(d3.axisBottom(x));
+      .call(d3.axisBottom(x).tickSize(2));
 
   svg.append("g")
       .attr("class", "axis axis--y")
       .attr("transform", "translate(1,0)")
-      .call(d3.axisLeft(y));
+      .call(d3.axisLeft(y)
+        .ticks(5)
+        .tickSize(2));
 
   const barWidth = (width / data.length);
   const bar = svg.selectAll(".bar")
@@ -85,67 +152,12 @@ const CONTAINER_W = parseFloat(d3.select('container').style('width'));
       .attr("height", (d) => height - y(d))
       .attr('fill', 'rgb(162, 109, 125)');
 
-
   bar.append("text")
       .attr("dy", ".5em")
-      .attr("y", d => y(d) - 10  )
+      .attr("y", d => y(d) - 10 )
       .attr("x", barWidth / 2)
       .attr("text-anchor", "middle")
       .attr('class', 'text')
+      .attr('font-size', '13')
       .text(d => d);
-})();
-
-//***********linear graphics********//
-(function(){
-  const data = d3.range(20).map((e) => {
-    return {
-      x: Number(e),
-      y: +Math.round((Math.random() * 100) + 1)
-    }
-  });
-  const margin = {top: 10, right: 20, bottom: 20, left: 20},
-    width = CONTAINER_W / 4 - margin.left - margin.right,
-    height = CONTAINER_W / 8 - margin.top - margin.bottom;
-
-  const x = d3.scaleTime()
-      .range([0, width])
-      .domain([0, data.length]);
-
-  const y = d3.scaleLinear()
-      .rangeRound([height, 0])
-      .domain([0, d3.max(data, function(d) {return d.y})]);
-
- const line = d3.line()
-      .x(function(d) {return x(d.x)})
-      .y(function(d) {return y(d.y)})
-      .curve(d3.curveBasis)
-
-  const svg = d3.select("#columnChild56854").append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .attr('class','svgs')
-    .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-  svg.append("g")
-      .attr("class", "axis axis--x")
-      .attr("transform", "translate(1," + height + ")")
-      .call(d3.axisBottom(x).ticks(10));
-
-  svg.append("g")
-      .attr("class", "axis axis--y")
-      .attr("transform", "translate(1,0)")
-      .call(d3.axisLeft(y).ticks(5))
-    .select(".domain")
-      .remove();
-
-  svg.append('path')
-     .datum(data)
-     .attr('fill', 'none')
-     .attr('stroke', 'black')
-     .attr('stroke-width', 1)
-     .attr('stroke-linejoin', 'round')
-     .attr('stroke-linecap', 'round')
-     .attr('stroke-dasharray', '5 2')
-     .attr('d', line);
 })();
