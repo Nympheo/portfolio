6ef0fa9df5186629d4a3c8d3daf94763f8ad7000
Nympheo <synthetic.probe@gmail.com>
2017-06-23 16:06:47 +0300

    tree added

--- js/myScript.js
+++ js/myScript.js
@@ -245,7 +245,7 @@ svg.selectAll('circle')
 
   const xScale = d3.scaleLinear()
       .domain([0, data.length])
-      .range([0, width + 20]);
+      .range([0, width + 10]);
 
   const area = d3.area()
     .x((d,i) => xScale(i))
@@ -263,7 +263,7 @@ svg.selectAll('circle')
 
  svg.append("g")
     .attr("class", "axis axis--x")
-    .attr("transform", "translate(-1," + height + ")")
+    .attr("transform", "translate(-1," + (height)  + ")")
     .call(d3.axisBottom(xScale).ticks(5).tickSize(1));
 
 svg.append("g")
@@ -280,7 +280,7 @@ svg.append("g")
 })();
 //**********SECTION 7****PIE*********************
 (function(){
-  const margin = {top: 3, right: 3, bottom: 3, left: 3},
+  const margin = {top: 0, right: 0, bottom: 0, left: 15},
     width = CONTAINER_W / 16 - margin.left - margin.right,
     height = CONTAINER_W / 16 - margin.top - margin.bottom;
 
@@ -291,7 +291,7 @@ svg.append("g")
     .append("g")
       .attr("transform", `translate(
             ${(width + margin.right + margin.left) / 2},
-            ${(height + margin.top + margin.bottom) / 2})`);
+            ${(height - 15) / 2})`);
 
  const names = ['1', '2', '3', '4', '5', '6', '7'];
  const data = d3.range(7).map(e => Math.floor(Math.random() * 30));
@@ -343,9 +343,9 @@ svg.selectAll('text')
       });
 
 })();
-//*************SECTION 8****HEXAGONAL function*******************************//
+//*************SECTION 8****PACK (HEXAGONAL) function*******************************//
 (function(){
-  const margin = {top: 3, right: 3, bottom: 3, left: 3},
+  const margin = {top: -5, right: 0, bottom: 0, left: 5},
     width = CONTAINER_W / 16 - margin.left - margin.right,
     height = CONTAINER_W / 16 - margin.top - margin.bottom;
 
@@ -356,47 +356,82 @@ svg.selectAll('text')
     .append("g")
       .attr("transform", `translate(${margin.left},${margin.top})`);
 
-const randomX = d3.randomNormal(width / 2, 10);
-const randomY = d3.randomNormal(height / 2, 10);
-const points = d3.range(100).map(e => [randomX(), randomY()]);
-const color = d3.scaleSequential(d3.interpolateLab("rgb(219, 204, 214)","rgb(122, 29, 77)"))
-                .domain([0, 10]);
+  const data = {name: "A1",
+                children: [{name: "B1",
+                            children: [{name: "C1", value: 100},
+                                       {name: "C2", value: 300},
+                                       {name: "C3", value: 200}]
+                           },
+                           {name: "B2",value: 200}
+                          ]
+               };
 
-const hexbin = d3.hexbin()
-                 .radius(5)
-                 .extent([[0, 0],[width, height]]);
+const packLayout = d3.pack().size([width - 10, height - 10]);
+const hierarchy = d3.hierarchy(data);
+      hierarchy.sum(d => d.value);
+      packLayout(hierarchy);
 
-const x = d3.scaleLinear()
-            .domain([0, width])
-            .range([0, width]);
-const y = d3.scaleLinear()
-            .domain([0, height])
-            .range([height, 0]);
+const nodes = svg.selectAll('circle')
+      .data(hierarchy.descendants())
+      .enter()
+      .append('g')
+      .attr(`transform`, function(d){return `translate(${d.x},${d.y})`});
 
-svg.append('clipPath')
-   .attr('id', 'clip')
-   .append('rect')
-      .attr('width', width)
-      .attr('height', height);
+nodes.append('circle')
+     .attr('r', d => d.r)
+     .attr('fill', 'indianred')
+     .attr('opacity', 0.3)
+     .attr('stroke', 'black');
 
-svg.append('g')
-   .attr('stroke', '#000')
-   .attr('stroke-width', '0.5px')
-   .attr('clip-path', 'url(#clip)')
-   .selectAll('path')
-   .data(hexbin(points))
-   .enter().append('path')
-       .attr('d', hexbin.hexagon())
-       .attr('transform', function(d){ ;return `translate(${d.x},${d.y})` })
-       .attr('fill', d => color(d.length))
+nodes.append('text')
+     .attr('dy', 4)
+     .attr('fill', 'black')
+     .attr('font-size', '.55em')
+     .attr('text-anchor', 'middle')
+     .text(d => d.children === undefined ? d.data.name : ``);
+
+//-----------------------HEX---------------
+// const randomX = d3.randomNormal(width / 2, 10);
+// const randomY = d3.randomNormal(height / 2, 10);
+// const points = d3.range(100).map(e => [randomX(), randomY()]);
+// const color = d3.scaleSequential(d3.interpolateLab("rgb(219, 204, 214)","rgb(122, 29, 77)"))
+//                 .domain([0, 10]);
+//
+// const hexbin = d3.hexbin()
+//                  .radius(5)
+//                  .extent([[0, 0],[width, height]]);
+//
+// const x = d3.scaleLinear()
+//             .domain([0, width])
+//             .range([0, width]);
+// const y = d3.scaleLinear()
+//             .domain([0, height])
+//             .range([height, 0]);
+//
+// svg.append('clipPath')
+//    .attr('id', 'clip')
+//    .append('rect')
+//       .attr('width', width)
+//       .attr('height', height);
+//
+// svg.append('g')
+//    .attr('stroke', '#000')
+//    .attr('stroke-width', '0.5px')
+//    .attr('clip-path', 'url(#clip)')
+//    .selectAll('path')
+//    .data(hexbin(points))
+//    .enter().append('path')
+//        .attr('d', hexbin.hexagon())
+//        .attr('transform', function(d){ ;return `translate(${d.x},${d.y})` })
+//        .attr('fill', d => color(d.length))
 })();
-//***************SECTION 3************CHORD DIAGRAM***************************//
+//***************SECTION 5************CHORD DIAGRAM***************************//
 (function(){
-  const margin = {top: 5, right: 0, bottom: 5, left: 0},
-    width = CONTAINER_W / 4 - margin.left - margin.right,
-    height = CONTAINER_W / 4 - margin.top - margin.bottom;
+  const margin = {top: 0, right: 0, bottom: 5, left: 0},
+    width = CONTAINER_W / 8 - margin.left - margin.right,
+    height = CONTAINER_W / 8 - margin.top - margin.bottom;
 
-  const svg = d3.select("#rowChild46489").append("svg")
+  const svg = d3.select("#rowChild68118").append("svg")
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
       .attr('class','svgs')
@@ -408,14 +443,14 @@ const matrix = [[14632,  4312, 4123, 1234],
                 [ 5123, 19765, 4564, 9443],
                 [ 312,   5086,  900, 7532]];
 
-const outerRadius = Math.min(width, height) / 2 - 10,
-      innerRadius = outerRadius - 30;
+const outerRadius = Math.min(width, height) / 2 - 20,
+      innerRadius = outerRadius - 9;
 
-const formatValue = d3.formatPrefix(',.0', 1e3);
+const formatValue = d3.formatPrefix('.0', 1000);
 
 const chord = d3.chord()
-                .padAngle(0.04)
-                .sortSubgroups(d3.descending);
+                .padAngle(0.1)
+                .sortSubgroups(d3.ascending);
 
 const arc = d3.arc()
               .innerRadius(innerRadius)
@@ -431,7 +466,6 @@ const color = d3.scaleOrdinal()
 
 svg.datum(chord(matrix));
 const group = svg.append('g')
-                 .attr('class', 'groups')
                  .selectAll('g')
                     .data(d => d.groups)
                     .enter().append('g');
@@ -444,37 +478,169 @@ group.append('path')
 const ticks = group.selectAll('.tick')
       .data(d => tickFunc(d, 1000))
       .enter().append('g')
-        .attr('stroke', '#000')
         .attr('transform', function(d){
             return `rotate(${d.angle * 180 / Math.PI - 90})
                     translate(${outerRadius}, 0)`});
 
-ticks.append('line').attr('x2', 2);
+ticks.append('line').attr('x2', 1).attr('stroke', '#000');
 
 ticks.filter(d => d.value % 5000 === 0)
      .append('text')
-     .attr('x', 7)
+     .attr('x', function(d){return d.angle > Math.PI ? '15' : '2'})
      .attr('dy', '.35em')
-     .attr('font-size', '.44em')
+     .attr('font-size', '.54em')
      .attr('transform', function(d){
-       return d.angle > Math.PI ? `rotate(180) translate(-16)` : null;})
+       return d.angle > Math.PI ? `rotate(180) translate(-18)` : null;})
      .style('text-anchor', d => d.angle > Math.PI ? 'end' : null)
      .text(d => formatValue(d.value));
 
 svg.append('g')
-   .attr('fill-opacity', 0.7)
+   .attr('fill-opacity', 0.3)
    .selectAll('path')
    .data(d => d)
    .enter().append('path')
       .attr('d', ribbon)
-      .style('fill', d => d.target.index)
+      .style('fill', d => color(d.target.index))
       .style('stroke', d => d3.rgb(color(d.target.index)).darker());
 
 function tickFunc(d, step) {
    let i = (d.endAngle - d.startAngle) / d.value;
    return d3.range(0, d.value, step).map(function(e)
      {return {value: e, angle: e * i + d.startAngle}})
-};
+   };
+})();
+//************************** TREE ****SECTION 3***********************//
+(function(){
+  const data = [
+    {id:'President',},
+    {id:'President.Finance',},
+    {id:'President.Finance.Receivables',},
+    {id:'President.Finance.Receivables.Staff1',},
+    {id:'President.Finance.Receivables.Staff2', value: 393},
+    {id:'President.Finance.Receivables.Staff3', value: 381},
+    {id:'President.Finance.Receivables.Staff4', value: 381},
+    {id:'President.Finance.Receivables.Staff5', value: 381},
+    {id:'President.Finance.Receivables.Staff6', value: 381},
+    {id:'President.Finance.Payables',},
+    {id:'President.Finance.Payables.Staff1',},
+    {id:'President.Finance.Payables.Staff2',value: 144},
+    {id:'President.Finance.Payables.Staff3',value: 133},
+    {id:'President.Finance.Payables.Staff4',value: 155},
+    {id:'President.Finance.Payables.Staff5',value: 166},
+    {id:'President.Market',},
+    {id:'President.Market.Market R/D', },
+    {id:'President.Market.Market R/D.Staff1', value: 353},
+    {id:'President.Market.Market R/D.Staff2', value: 573},
+    {id:'President.Market.Market R/D.Staff3', value: 573},
+    {id:'President.Market.Market R/D.Staff4', value: 573},
+    {id:'President.Market.Staff1', value: 784},
+    {id:'President.Market.Staff2', value: 591},
+    {id:'President.Market.Staff3', value: 341},
+    {id:'President.Market.Advertising',},
+    {id:'President.Market.Advertising.Staff1', },
+    {id:'President.Market.Advertising.Staff2', value: 707},
+    {id:'President.Market.Advertising.Staff3', value: 707},
+    {id:'President.Optns',},
+    {id:'President.Optns.Production',},
+    {id:'President.Optns.Production.Staff1', },
+    {id:'President.Optns.Production.Staff2', value: 137},
+    {id:'President.Optns.Production.Staff3', value: 444},
+    {id:'President.Optns.Production.Staff4', value: 137},
+    {id:'President.Optns.Production.Staff5', value: 555},
+    {id:'President.Optns.Production.Staff6', value: 137},
+    {id:'President.Optns.Production.Staff7', value: 666},
+    {id:'President.Optns.Production.Staff8', value: 137},
+    {id:'President.Optns.Production.Staff9', value: 777},
+    {id:'President.Optns.Delivery'},
+    {id:'President.Optns.Delivery.Staff1', value: 137},
+    {id:'President.Optns.Delivery.Staff2', value: 137},
+    {id:'President.Optns.Delivery.Staff3', value: 137},
+    {id:'President.Optns.Delivery.Staff4', value: 137},
+    {id:'President.Optns.Delivery.Staff5', value: 137},
+    {id:'President.Optns.Delivery.Staff6', value: 137},
+  ];
 
+const margin = {top: -10, right: -10, bottom: 10, left: 10},
+    width = CONTAINER_W / 4 - margin.left - margin.right,
+    height = CONTAINER_W / 4 - margin.top - margin.bottom;
+
+const svg = d3.select("#rowChild46489").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .attr('class','svgs')
+    .append("g")
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+const stratify = d3.stratify()
+      .parentId(d => d.id.substring(0,d.id.lastIndexOf('.')));
+
+const tree = d3.tree()
+      .size([2 * Math.PI, 120])
+      .separation((a, b) => (a.parent == b.parent ? 1 : 2) / a.depth);
+
+const root = tree(stratify(data));
+
+const link = svg.selectAll('.link')
+        .data(root.links())
+        .enter().append('path')
+          .attr('fill', 'none')
+          .attr('stroke', '#555')
+          .attr('stroke-opacity', 0.9)
+          .attr('stroke-width', '1px')
+          .attr('d', d3.linkRadial()
+                        .angle(d => d.x)
+                        .radius(d => d.y));
+
+const node = svg.selectAll('.node')
+      .data(root.descendants())
+      .enter().append('g')
+          // .attr('class', d => d.children ? 'node-internal' : 'node-leaf')
+          .attr('transform', function(d) {
+            return `translate(${radians(d.x, d.y)})`
+          });
+
+node.append('circle').attr('r', 2.5).attr('fill', '#803843');
+
+node.append('text')
+    .attr('dy', '0.3em')
+    .attr('x', d => d.x < Math.PI === !d.children ? 7 : -3)
+    .attr('text-anchor', d => d.x < Math.PI === !d.children ? 'start' : 'end')
+    .attr('transform',d => `rotate(${(d.x<Math.PI?d.x-Math.PI/2:d.x+Math.PI/2)*180/Math.PI})`)
+    .text(d => d.id.substring(d.id.lastIndexOf('.') + 1))
+    .attr('font-size', '.55em');
+
+function radians(x, y) {
+  return [(y = +y) * Math.cos( x -= Math.PI / 2), y * Math.sin(x)];
+}
+
+// const hierarchy = d3.hierarchy(data);
+//
+// const treeLayout = d3.tree().size([width  , height ]);
+//
+// treeLayout(hierarchy);
+//
+// const links = svg.append('g')
+//     .selectAll('line')
+//     .data(hierarchy.links())
+//     .enter()
+//     .append('line')
+//     .attr('x1', d => d.source.x)
+//     .attr('y1', d => d.source.y)
+//     .attr('x2', d => d.target.x)
+//     .attr('y2', d => d.target.y)
+//     .attr('fill', 'none')
+//     .attr('stroke', '#000')
+//     .attr('stroke-width', '1px');
+//
+// const nodes = svg.append('g')
+//     .selectAll('circle')
+//     .data(hierarchy.descendants())
+//     .enter()
+//     .append('circle')
+//     .attr('cx', d => d.x)
+//     .attr('cy', d => d.y)
+//     .attr('fill', 'rgb(173, 101, 114)')
+//     .attr('stroke', 'none')
+//     .attr('r', 3);
 
 })();
